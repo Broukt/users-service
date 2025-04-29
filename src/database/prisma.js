958152f02d5d43b74fd9be7d2bc67e140e8a51d0
Jairo Calcina <j.calcina.v@gmail.com>
2025-04-28 21:17:38 -0400

    feat: add prisma setup

---
+++ src/database/prisma.js
@@ -0,0 +1,9 @@
+const { PrismaClient } = require("@prisma/client");
+let prisma;
+if (process.env.NODE_ENV === "production") prisma = new PrismaClient();
+else {
+  if (!global.prisma)
+    global.prisma = new PrismaClient({ log: ["query", "warn", "error"] });
+  prisma = global.prisma;
+}
+module.exports = prisma;
