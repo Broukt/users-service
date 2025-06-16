0e0e30173286d05d5e43819308aee9125aa47dfb
Jairo Calcina <j.calcina.v@gmail.com>
2025-06-15 23:11:31 -0400

    feat: add service middlewares

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 const { Server } = require("@grpc/grpc-js");
 const initializeConsumers = require("./rabbitmq/initialize");
-const { grpcErrorHandler } = require("./middlewares/grpcErrorHandlerMiddleware");
+const {
+  grpcErrorHandler,
+} = require("./middlewares/grpcErrorHandlerMiddleware");
 const loadProto = require("./utils/loadProto");
 const userService = require("./services/userService");
+const { validateUpdateUser } = require("./middlewares/grpcValidatorMiddleware");
+const {
+  grpcEnsureAdmin,
+  grpcEnsureSelfOrAdmin,
+} = require("./middlewares/grpcAuthMiddleware");
 
 const server = new Server();
 const proto = loadProto("users");
-server.addService(proto.Users.service, userService);
+server.addService(proto.Users.service, {
+  GetUser: grpcEnsureSelfOrAdmin(userService.GetUser),
+  UpdateUser: validateUpdateUser(userService.UpdateUser),
+  DeleteUser: grpcEnsureAdmin(userService.DeleteUser),
+  ListUsers: grpcEnsureAdmin(userService.ListUsers),
+});
 
 // Inicializa manejo global de errores y consumidores de RabbitMQ
 grpcErrorHandler(server);
