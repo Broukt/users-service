fb6d728737e76c7244c927f13da095730c5556be
Jairo Calcina <j.calcina.v@gmail.com>
2025-07-03 02:49:42 -0400

    chore: modify environment variables

--- src/config/env.js
+++ src/config/env.js
@@ -4,10 +4,15 @@ module.exports = {
   port: process.env.PORT || 50051,
   serverUrl: process.env.SERVER_URL || "0.0.0.0",
   rabbitUrl: process.env.RABBITMQ_URL || "amqp://localhost",
-  userCreatedQueue: process.env.USER_CREATED_QUEUE || "user.created",
+  userCreatedQueueAuth:
+    process.env.USER_CREATED_QUEUE_AUTH || "user.created.auth",
+  userCreatedQueueBilling:
+    process.env.USER_CREATED_QUEUE_BILLING || "user.created.billing",
   userUpdatedQueue: process.env.USER_UPDATED_QUEUE || "user.updated",
-  userDeletedQueue: process.env.USER_DELETED_QUEUE || "user.deleted",
-  roleCreatedQueue: process.env.ROLE_CREATED_QUEUE || "role.created",
-  roleUpdatedQueue: process.env.ROLE_UPDATED_QUEUE || "role.updated",
-  roleDeletedQueue: process.env.ROLE_DELETED_QUEUE || "role.deleted",
+  userDeletedQueues: [
+    process.env.USER_DELETED_QUEUE_AUTH || "user.deleted.auth",
+    process.env.USER_DELETED_QUEUE_BILLING || "user.deleted.billing",
+  ],
+  exchangeUserDeleted:
+    process.env.EXCHANGE_USER_DELETED || "exchange.user.deleted",
 };
