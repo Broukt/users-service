e33843fcfd5617680b2695780d49f28a0001431e
Jairo Calcina <j.calcina.v@gmail.com>
2025-07-03 02:56:16 -0400

    feat: add update user validator

--- src/app.js
+++ src/app.js
@@ -1,27 +1,22 @@
 const { Server } = require("@grpc/grpc-js");
-const initializeConsumers = require("./rabbitmq/initialize");
 const {
   grpcErrorHandler,
 } = require("./middlewares/grpcErrorHandlerMiddleware");
 const loadProto = require("./utils/loadProto");
 const userService = require("./services/userService");
 const { validateUpdateUser } = require("./middlewares/grpcValidatorMiddleware");
-const {
-  grpcEnsureAdmin,
-  grpcEnsureSelfOrAdmin,
-} = require("./middlewares/grpcAuthMiddleware");
 
 const server = new Server();
 const proto = loadProto("users");
 server.addService(proto.Users.service, {
-  GetUser: grpcEnsureSelfOrAdmin(userService.GetUser),
+  CreateUser: userService.CreateUser,
+  GetUser: userService.GetUser,
   UpdateUser: validateUpdateUser(userService.UpdateUser),
-  DeleteUser: grpcEnsureAdmin(userService.DeleteUser),
-  ListUsers: grpcEnsureAdmin(userService.ListUsers),
+  DeleteUser: userService.DeleteUser,
+  ListUsers: userService.ListUsers,
 });
 
 // Inicializa manejo global de errores y consumidores de RabbitMQ
 grpcErrorHandler(server);
-initializeConsumers().then(() => console.log("🐇 Consumers ready"));
 
 module.exports = server;
